b0f3cc8b1dfeb6ad9cb17646e101f3da09455bd8
TanSomnang <34526950+TanSomnang@users.noreply.github.com>
2018-06-27 14:38:53 +0700

    change1

--- SquareNumber/src/SquareNumber.scala
+++ SquareNumber/src/SquareNumber.scala
@@ -16,32 +16,36 @@ object SquareNumber {
       
       
      
+      println("Please, enter a number to convert it to square: ")
+      var n3=scala.io.StdIn.readInt()
+         
       var ex=true
        while(ex)
        {
-          println("Please, enter a number: ")
+          println("Please, enter a guessing number: ")
           var input=scala.io.StdIn.readInt()
-          var n3=scala.io.StdIn.readInt()
+          
           
           var sq3=(n3:Int) => n3*n3
           
-          if(input=>sq3)
+          if(input==sq3(n3))
           {
             println("Your answer is correct.")
             ex=false
           }
-          
+          else if(input==sq3(n3)-1 || input==sq3(n3)+1)
+          {
+            println("Your answer is nearly correct. But it's ok.")
+            ex=false
+          }
           else
           {
-            println("Your answer is wrong.")
+            println("Your answer "+input+ " is wrong.")
             ex=true
           }
          
          
        }
-        
-      
-      
-    }
+  }
     
   }
